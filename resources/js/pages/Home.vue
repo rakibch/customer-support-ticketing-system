54e31ff2ade4aa4a30c52516c6e6e9134aa0909c
rakibch <rakib9204@gmail.com>
2025-05-08 08:35:52 +0600

    integrated vuejs and vite at laravel

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>Welcome to the Ticketing System</div>
+  </template>
+  
+  <script lang="ts" setup></script>
+  
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/js/app.ts'],
+      refresh: true,
+    }),
+    vue(),
+  ],
 });
